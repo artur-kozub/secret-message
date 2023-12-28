e631b8b8bb8ddfa1187a0f0ad2f8da279a695857
Arthur Kozub <kozub0246@gmail.com>
2023-12-28 14:54:53 +0200

    some changes in index

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const phrases = require('./helper')
 const TelegramApi = require('node-telegram-bot-api')
 const heartEmoji = '\u2764\uFE0F';
-const token = 'your-bot-token'
+const inLoveEmoji = '\u{1F60D}'
+const token = '6877742053:AAFlDwXpoJiiuCL_tUpiCEqKFzQN9FHVqyM'
 
 const bot = new TelegramApi(token, { polling: true })
 
@@ -11,12 +12,19 @@ const messageGenerator = (arr) => {
     return arr[randInt];
 }
 
-bot.on('message', msg => {
+bot.setMyCommands([
+    { command: '/start', description: 'Для початку' },
+    { command: '/info', description: 'Інформація про бот' },
+])
+
+bot.on('message', async msg => {
     const text = msg.text;
     const chatId = msg.chat.id;
     const message = messageGenerator(phrases);
     if (text === '/start') {
-        bot.sendMessage(chatId, `Привіт, Настя ${heartEmoji}, напиши мені будь що :)`)
-    } else {bot.sendMessage(chatId, message);}
+        await bot.sendSticker(chatId, 'https://chpic.su/_data/stickers/c/Cats_stikeru7777/Cats_stikeru7777_008.webp?v=1703065210')
+        await bot.sendMessage(chatId, `Привіт, Настя ${heartEmoji}, напиши мені будь що ${inLoveEmoji}`)
+    } else if (text === '/info') {await bot.sendMessage(chatId,'Я створив цей бот для того щоб тобі не було сумно, якщо мене не буде поруч' + inLoveEmoji + 'поки що він реагує тільки коли ти надсилаєш повідомлення, це може будь-що, навіть літера або картинка чи файл')} 
+    else { await bot.sendMessage(chatId, message); }
     console.log(msg);
 });
